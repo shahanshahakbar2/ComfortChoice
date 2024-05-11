19a9829a0ae4dad81f6fe6bd00094a405411a1b4
Aftab Alam <aftabf8982@gmail.com>
2024-05-11 23:25:37 +0530

    added nevigation using react-router-dom and modified replace image from Navbar to Aside component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./navbar.css";
 import logo from "../../assests/logo.webp";
-import image1 from "../../assests/image1.webp";
 import { CiSearch } from "react-icons/ci";
 import { GrUserManager } from "react-icons/gr";
 import { IoBagSharp } from "react-icons/io5";
+import { Link } from "react-router-dom";
 import Shop from "../../container/shop/Shop";
 
 const Navbar = () => {
@@ -12,32 +12,31 @@ const Navbar = () => {
     <>
       <div className='links container'>
         <div className='text-container1'>
-          <p>Shop</p>
-          <p>Our Story</p>
-          <p>Contact</p>
+          <Link to='shop' className='text'>
+            SHOP
+          </Link>
+          <Link to='our-story' className='text'>
+            OUR STORY
+          </Link>
+          <Link to='contact' className='text'>
+            CONTACT
+          </Link>
         </div>
 
         <div className='text-container2'>
-          <a href='#contact'>
-            <img src={logo} alt='logo' />
-          </a>
+          <img src={logo} alt='logo' />
         </div>
         <div className='text-container3'>
           <div className='search-container'>
             <CiSearch />
             <input type='text' name='' placeholder='Search...' />
           </div>
-          <p>
+          <div className='login-container'>
             <GrUserManager />
-            Login
-          </p>
-          <p>
+            <p>Log In</p>
             <IoBagSharp />
-          </p>
+          </div>
         </div>
-      </div>
-      <div className='image'>
-        <img src={image1} alt='pic' />
       </div>
     </>
   );
---
+++ src/container/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Aside, Product, Section } from "../../components";
+
+const Home = () => {
+  return (
+    <>
+      <Aside />
+      <Product />
+      <Section />
+    </>
+  );
+};
+
+export default Home;
