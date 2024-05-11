89b70f7680e01e6c24b44c9c442c880a5cb6c09a
Aftab Alam <aftabf8982@gmail.com>
2024-05-11 16:05:24 +0530

    added alt prop in Product component and modified Search bar in Navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,31 +1,46 @@
-import React from 'react';
-import './navbar.css';
-import logo from '../../assests/logo.webp';
-import image1 from '../../assests/image1.webp';
+import React from "react";
+import "./navbar.css";
+import logo from "../../assests/logo.webp";
+import image1 from "../../assests/image1.webp";
 import { CiSearch } from "react-icons/ci";
 import { GrUserManager } from "react-icons/gr";
 import { IoBagSharp } from "react-icons/io5";
-import Shop from '../../container/shop/Shop'
-
+import Shop from "../../container/shop/Shop";
 
 const Navbar = () => {
-    return (
-        <>
-            <div className='links'>
-                <li><a href={Shop}>Shop</a></li>
-                <li><a href='#ourstory'>Our Story</a></li>
-                <li><a href='#contact'>Contact</a></li>
-                <li><a href='#contact'><img src={logo} alt='logo' /></a></li>
-                <li><a href='#contact'><CiSearch />search</a></li>
-                <li><a href='#contact'><GrUserManager />Login</a></li>
-                <li><a href='#contact'><IoBagSharp /></a></li>
-            </div>
-            <div className='image'>
-<img src={image1} alt='pic' />
-            </div>
+  return (
+    <>
+      <div className='links container'>
+        <div className='text-container1'>
+          <p>Shop</p>
+          <p>Our Story</p>
+          <p>Contact</p>
+        </div>
 
-        </>
-    )
-}
+        <div className='text-container2'>
+          <a href='#contact'>
+            <img src={logo} alt='logo' />
+          </a>
+        </div>
+        <div className='text-container3'>
+          <div className='search-container'>
+            <CiSearch />
+            <input type='text' name='' placeholder='Search...' />
+          </div>
+          <p>
+            <GrUserManager />
+            Login
+          </p>
+          <p>
+            <IoBagSharp />
+          </p>
+        </div>
+      </div>
+      <div className='image'>
+        <img src={image1} alt='pic' />
+      </div>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
