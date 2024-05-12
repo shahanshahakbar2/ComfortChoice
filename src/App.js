2d6f04618a26d08ff018c3caaaece59fc2ee385c
Aftab Alam <aftabf8982@gmail.com>
2024-05-12 23:20:19 +0530

    replace container name with routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React from "react";
 import { Header, Footer, Navbar } from "./components";
 import "./App.css";
-import Home from "./container/home/Home";
+import Home from "./routes/home/Home";
 import { Route, Routes } from "react-router-dom";
-import Shop from "./container/shop/Shop";
-import Contact from "./container/contact/Contact";
-import OurStory from "./container/our-story/OurStory";
-import Login from "./container/login/Login";
+import Shop from "./routes/shop/Shop";
+import Contact from "./routes/contact/Contact";
+import OurStory from "./routes/our-story/OurStory";
+import Login from "./routes/login/Login";
+import Cart from "./routes/cart/Cart";
 
 const App = () => (
   <div className='App'>
     <Header />
     <Navbar />
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='shop' element={<Shop />} />
-      <Route path='our-story' element={<OurStory />} />
-      <Route path='contact' element={<Contact />} />
-      <Route path='login' element={<Login />} />
+      <Route path='/' element={<Home />}>
+        <Route path='shop' element={<Shop />} />
+        <Route path='our-story' element={<OurStory />} />
+        <Route path='contact' element={<Contact />} />
+        <Route path='login' element={<Login />} />
+      </Route>
+      <Route path='cart' element={<Cart />} />
     </Routes>
     <Footer />
   </div>
